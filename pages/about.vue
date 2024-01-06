6c2af0d84c1b03dc5cc44296edf02996b64ebd7e
6310682643 <6310682643@student.tu.ac.th>
2024-01-07 01:42:43 +0700

    update2

--- pages/about.vue
+++ pages/about.vue
@@ -1,22 +1,57 @@
+
 <template>
     <v-app style="background-color:rgb(236, 236, 236);">
         <NavBar />
+        <Nuxt />
         <h1>About</h1>
-        
+        <!-- <button @click="makePayment">Pay Now</button>
+        <div>
+            <stripe-checkout
+            ref="checkoutRef"
+            mode="payment"
+            :pk="publishableKey"
+            :line-items="lineItems"
+            :success-url="successURL"
+            :cancel-url="cancelURL"
+            @loading="v => loading = v"
+            />
+            <button @click="submit">pay!</button>
+        </div> -->
     </v-app>
-    
+
 </template>
-
 <script lang="ts">
-import NavBar from "../components/NavBar.vue"
-import SignIn from "../components/SignIn.vue"
+import { StripeCheckout } from '@vue-stripe/vue-stripe';
 
-export default {
+export default ({
     name: "about",
-    components: { NavBar, SignIn},
-}
+    components: {StripeCheckout, },
+    
+//     data () {
+//     this.publishableKey = "pk_test_51OPNLwFJUwe1va09pikfEhMZZw3SrXulpaqGMXQbeT9kTm2MB6nbWKNWPNcTe3OJ1fJHw5a0d3H6TzA73NS3Ykjk003g6rTcC7";
+//     return {
+//       loading: false,
+//       lineItems: [
+//         {
+//           price: 'price_1OUwbYFJUwe1va094z3E6lVZ',
+//           quantity: 1,
+          
+//         },
+//       ],
+//       successURL: 'http://localhost:3000/',
+//       cancelURL: 'http://localhost:3000/',
+//     };
+//   },
+//   methods: {
+//     submit () {
+//       // You will be redirected to Stripe's secure checkout page
+//       this.$refs.checkoutRef.redirectToCheckout();
+//     },
+//   },
+    
+});
 </script>
 
 <style>
 
-</style>
+</style> 
